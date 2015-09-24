87a84adfad2681a108162d1a5263b194c8e6df96
Antti Kekki <antti.kekki@eficode.com>
2015-09-24 21:40:58 +0300

    Hitaan suorituskykydemon piilotus ettei lataus kestä

--- js/app/suorituskyky/suorituskyky.controller.js
+++ js/app/suorituskyky/suorituskyky.controller.js
@@ -36,4 +36,40 @@ angular.module('suorituskyky')
   	return orderValue;
   };
 
+})
+.controller('SuorituskykyController2', function($scope) {
+  var controller = this;
+  $scope.customers = [];
+  $scope.orders = [];
+  $scope.orderValueForCustomerIdMap = {};
+
+  var count = 1;
+  while(count < 50) {
+    var orderCount = 1;
+    while(orderCount < 5000) {
+      $scope.orders.push({
+        customerId: count,
+        value: orderCount * count
+      });
+      orderCount++;
+    }
+
+    $scope.customers.push({
+      id: count,
+      name: 'Asiakas '+count
+    });
+    count++;
+  }
+
+  controller.countOrderValues = function() {
+    $scope.customers.forEach(function(customer) {
+      var orderValue = 0;
+      $scope.orders
+      .filter(function(o) { return o.customerId === customer.id})
+      .forEach(function(o) { orderValue += o.value });
+
+      $scope.orderValueForCustomerIdMap[customer.id] = orderValue;
+    });
+  };
+  controller.countOrderValues();
 });
